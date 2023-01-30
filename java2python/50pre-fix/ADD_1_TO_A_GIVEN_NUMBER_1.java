0eed8aaa9303cf1b2c5a6d6a1ead89dfe40c2365
Katelyn Zhou <123290258+kzh23@users.noreply.github.com>
2023-01-29 16:52:01 -0800

    Add files via upload

---
+++ java2python/50pre-fix/ADD_1_TO_A_GIVEN_NUMBER_1.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+import java.util. *;
+import java.util.stream.*;
+import java.lang.*;
+import javafx.util.Pair;
+public class ADD_1_TO_A_GIVEN_NUMBER_1{
+static int f_gold ( int x ) {
+  return ( - ( ~ x ) ) ;
+}
+
+
+}
