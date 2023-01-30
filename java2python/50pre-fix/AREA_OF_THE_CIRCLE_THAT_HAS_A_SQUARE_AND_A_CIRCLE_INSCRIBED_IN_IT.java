0f05241caa4adf1c36452558c3166221458126f7
Katelyn Zhou <123290258+kzh23@users.noreply.github.com>
2023-01-29 16:52:46 -0800

    Add files via upload

---
+++ java2python/50pre-fix/AREA_OF_THE_CIRCLE_THAT_HAS_A_SQUARE_AND_A_CIRCLE_INSCRIBED_IN_IT.java
@@ -0,0 +1,17 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+import java.util. *;
+import java.util.stream.*;
+import java.lang.*;
+import javafx.util.Pair;
+public class AREA_OF_THE_CIRCLE_THAT_HAS_A_SQUARE_AND_A_CIRCLE_INSCRIBED_IN_IT{
+static float f_gold ( int a ) {
+  float area = ( float ) ( Math . PI * a * a ) / 4 ;
+  return area ;
+}
+}
